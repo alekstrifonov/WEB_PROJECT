2bae081b96ae17f53084d6f4e5d3ce25d23b96bb
Alex Trifonov <alekstrifonov64@gmail.com>
2026-01-28 20:44:17 +0200

    SQL

--- config/users.sql
+++ config/users.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 27, 2026 at 09:40 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Jan 28, 2026 at 07:34 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
